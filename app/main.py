a601b6f80926b105ffeb294f0244a886df3ef775
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2023-06-06 17:12:51 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,17 @@
+Solution:
+
+Fast API:
+
+The API should provide the following endpoints:
+
+1. Indexing endpoint:
+This endpoint should allow developers to index data in a search engine or database. It should accept a JSON body with the data to be indexed and return a response indicating whether the indexing process was successful or not.
+
+2. Automation endpoint:
+This endpoint should allow developers to automate the indexing process. It should accept a JSON body with details about the automation, such as what data to index and when to index it, and return a response indicating whether the automation was successful or not.
+
+3. Search endpoint:
+This endpoint should allow developers to execute search queries against the index. It should accept a JSON body with the search query and return a response containing the results of the query.
+
+4. Update endpoint:
+This endpoint should allow developers to update the search index. It should accept a JSON body with details about the updates and return a response indicating whether the update was successful or not.
